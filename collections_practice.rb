a9c786af208d956db5363b85d7b5b647d5e4b345
Jordan Kline <jordan.kline@gmail.com>
2018-01-06 02:18:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,7 +26,7 @@ end
 def reverse_array(array)
   # array.reverse
   return_array = []
-  array.each { | el | return_array.shift(el) }
+  array.each { | el | return_array.push(el) }
   return_array
 end
 
