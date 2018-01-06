665350a112ea839a8507525b2a1ab15a078d3593
Jordan Kline <jordan.kline@gmail.com>
2018-01-06 01:30:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
 
+def sort_array_asc(array)
+  array.sort do | a, b |
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  return array.sort do | a, b |
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do | a, b |
+    a.chars.length <=> b.chars.length
+  end
+end
+
+def swap_elements(array) # how to do with sort method?
+  new_array = []
+  new_array << array[0] << array[2] << array[1]
+  new_array
+end
+
+def 
